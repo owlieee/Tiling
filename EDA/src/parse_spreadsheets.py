0297ab7b99a4975e8baf1e46cc2469e56ed73616
Allie <alliruss@gmail.com>
2018-01-09 15:10:41 -0700

    updated EDA for python 3

--- EDA/src/parse_spreadsheets.py
+++ EDA/src/parse_spreadsheets.py
@@ -1,8 +1,10 @@
 import csv
 import os
 import pandas as pd
-files = ['Tiling Training Set/'+f for f in os.listdir('Tiling Training Set/') if f.split('.')[-1]=='csv']
+import numpy as np
+import matplotlib.pyplot as plt
 
+files = ['../data/Tiling Training Set/'+f for f in os.listdir('../data/Tiling Training Set/') if f.split('.')[-1]=='csv']
 
 df = pd.DataFrame(columns = ['gene', 'sample_type', 't_array', 'gene_array'])
 
@@ -43,7 +45,7 @@ genes = ['ABL1', 'AKT1', 'ALK', 'APC', 'ATM', 'BRAF', 'CDH1', 'CDKN2A',
        'RB1', 'RET', 'SMAD4', 'SMARCB1', 'SMO', 'SRC', 'STK11', 'TP53',
        'VHL']
 
-responder_t_changes = {'CDKN2A', 'NOTCH', 'EGFR', 'IDH1', 'KRAS', 'SMARCB1', 'APC', 'GNA11', 'PIK3CA'}
+responder_t_changes = {'CDKN2A', 'NOTCH1', 'EGFR', 'IDH1', 'KRAS', 'SMARCB1', 'APC', 'GNA11', 'PIK3CA'}
 responder_touch_t_changes = {'HRAS', 'SMO'}
 responder_rand_changes = {'ABL1', 'JAK3'}
 nonresponder_t_changes = {'CDKN2A', 'EGFR', 'IDH1', 'KRAS', 'SMARCB1', 'APC', 'PIK3CA'}
@@ -56,8 +58,8 @@ t_dict['responder'].update({g:responder_t for g in genes if g in responder_t_cha
 t_dict['responder'].update({g:responder_touch_t for g in genes if g in responder_touch_t_changes})
 
 for f in files:
-    sample_type = f.split('.')[0].split('-')[-1]
-    with open(f, 'rb') as csvfile:
+    sample_type = f.split('.csv')[0].split('-')[-1]
+    with open(f, 'rt') as csvfile:
         reader = csv.reader(csvfile, delimiter = ',')
         for row in reader:
             if row[0]=='':
@@ -100,18 +102,21 @@ nonresponder = ranges[ranges['sample_type']=='nonresponder'][['gene', 'nt_min', 
 nonresponder = nonresponder.rename(columns = {'nt_min': 'nonresp_min', 'nt_max': 'nonresp_max',
                             't_min': 'nonresp_tumorregion_min', 't_max': 'nonresp_tumorregion_max'})
 ranges = normal.merge(responder).merge(nonresponder)
-ranges.to_csv('gene_ranges.csv', index = False)
+ranges.to_csv('../data/gene_ranges.csv', index = False)
 
 df['nt_mean'] = df['gene_nt_array'].apply(lambda x: x.mean())
 df['nt_std'] = df['gene_nt_array'].apply(lambda x: x.std())
 df['t_mean'] = df['gene_t_array'].apply(lambda x: x.mean())
 df['t_std'] = df['gene_t_array'].apply(lambda x: x.std())
 
-for ix, row in df[df['sample_type']=='normal'].iterrows():
-    fig, ax = plt.subplots()
-    ax.hist(row['gene_nt_array'])
-    ax.set_title(row['gene'])
-    fig.show()
+plot = False
+if plot == True:
+    for ix, row in df[df['sample_type']=='normal'].iterrows():
+        fig, ax = plt.subplots()
+        ax.hist(row['gene_nt_array'])
+        ax.set_title(row['gene'])
+        fig.show()
+
 ranges = df.groupby(['sample_type', 'gene']).mean().reset_index()
 normal = ranges[ranges['sample_type']=='normal'][['gene', 'nt_min', 'nt_max']]
 normal = normal.rename(columns = {'nt_min': 'normal_min', 'nt_max': 'normal_max'})
